322ddec2d1cade378973a3b9feb94d0c69358546
evankou23-ship-it <evankou23@gmail.com>
2026-05-10 20:23:35 -0400

    test2
    dsddw

--- counter.js
+++ counter.js
@@ -1,17 +1,19 @@
-function inc() {
-    c = (c >= 10) ? 0 : c + 1; 
-    ci = (ci >= 10) ? 0 : ci + 1; 
-    update();
-}
-
-function dec() {
-    c = c > 0 ? c - 1 : 0; 
-    cd = (cd >= 10) ? 0 : cd + 1; 
-    update();
-}
-
-function update() {
-    incCount.textContent = ci; 
-    decCount.textContent = cd; 
-    count.textContent = c;     
-}
+let c = 0, ci = 0, cd = 0;
+        const count = document.getElementById("count");
+        const incCount = document.getElementById("incCount");
+        const decCount = document.getElementById("decCount");
+        function inc() {
+            c++;
+            ci = (ci >= 10) ? 0 : ci + 1;
+            update();
+        }
+        function dec() {
+            c = c > 0 ? c - 1 : 0;
+            cd = (cd >= 10) ? 0 : cd + 1;
+            update();
+        }
+        function update() {
+            count.textContent = c;
+            incCount.textContent = ci;
+            decCount.textContent = cd;
+        }
